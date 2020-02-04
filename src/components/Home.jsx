bb3c760258339c162aa8b81680c5fe3768cb06b0
Nicholas Crofts <nicholas.crofts@gmail.com>
2020-02-04 15:02:38 -0500

    added image and new text to Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,25 +1,23 @@
 import React, { Component } from 'react'
-import logo from '../logo.svg'
 
 
 export default class Home extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='homeHeader'>
+        <img src={require('../Synth01.jpg')} />
+        <div>
+          Instant Synthwave
+        </div>
+        <div>
+          by Nick
+        </div>
+        <div>
+          Turn any everyday situation into a proper Synthwave Rave with the Instant Synthwave device!
+        </div>
+        <ul>
+          Lorem ipsum and yadda yadda yadda
+        </ul>
       </div>
     )
   }
