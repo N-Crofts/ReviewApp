88f223f5e1dc4ffa30b81435d5b081b7bf9a2327
Nicholas Crofts <nicholas.crofts@gmail.com>
2020-02-04 14:42:10 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
+
+  // <div>
+  //   <Router>
+  //     <Switch>
+  //       <Route exact path='/' component={ Home } />
+  //    </Switch>
+  //   </Router>
+  // </div>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
